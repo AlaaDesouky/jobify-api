143bd249156d80fbd786c324ced41cc9a823449c
Desouky <alaa.desouky33@gmail.com>
2022-04-07 15:06:14 +0200

    Edit/Delete job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from '../models/Job.js'
 import { StatusCodes } from 'http-status-codes'
 import { BadRequestError, NotFoundError } from '../errors/index.js'
+import checkPermission from '../utils/checkPermissions.js'
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId })
@@ -18,10 +19,33 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job })
 }
 const updateJob = async (req, res) => {
-  res.send('updateJob')
+  const { id: jobId } = req.params
+  const { company, position } = req.body
+
+  if (!company, !position) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const job = await Job.findOne({ _id: jobId })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+
+  checkPermission(req.user, job.createdBy)
+
+  const updateJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, { new: true, runValidators: true })
+  res.status(StatusCodes.OK).json({ updateJob })
 }
 const deleteJob = async (req, res) => {
-  res.send('deleteJob')
+  const { id: jobId } = req.params
+  const job = await Job.findOne({ _id: jobId })
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`)
+  }
+
+  checkPermission(req.user, job.createdBy)
+
+  await job.remove()
+  res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' })
 }
 const showStatus = async (req, res) => {
   res.send('showStatus')
